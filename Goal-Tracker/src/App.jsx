f4a6821c2e81759135f5a3e7cd012bdf1f44d4eb
Alex Marquez <marquez798@gmail.com>
2023-09-24 23:26:54 -0500

    sort of created nav bar

--- Goal-Tracker/src/App.jsx
+++ Goal-Tracker/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
 import BucketList from './components/BucketList';
+import Navbar from './components/Navbar';
 
 function App() {
+  
   return (
+    
     <div className="bucket-app">
+      <Navbar/>
       <BucketList />
+      
     </div>
   );
 }
---
+++ Goal-Tracker/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+export default function Navbar(){
+    return <nav className="nav">
+        <a href="/" className="site-tittle">about</a>
+        <ul>
+            <li>
+                <a href="/calender">calender</a>
+               
+            </li>
+            <li>
+                 <a href="/stats">stats</a>
+            </li>
+        </ul>
+
+    </nav>
+}
