7e7bd36571b67d54483ca93094a87fa25035faa5
Alex Marquez <marquez798@gmail.com>
2023-09-25 00:00:46 -0500

    setting up calender

--- Goal-Tracker/src/App.jsx
+++ Goal-Tracker/src/App.jsx
@@ -1,16 +1,20 @@
 import './App.css';
 import BucketList from './components/BucketList';
 import Navbar from './components/Navbar';
+import {useState} from 'react';
+import Calendar from 'react-calendar'; 
 
 function App() {
+  const [date, setDate] = useState(new Date())
+
   
   return (
     
     <div className="bucket-app">
       <Navbar/>
       <BucketList />
-      
     </div>
+    
   );
 }
 
